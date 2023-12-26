04f377494dabbdd71acc0a8225b04f7fdfeafa80
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2023-12-26 17:20:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -161,40 +161,6 @@ president_patterns04 = [
     re.compile(r"두[ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎabcdefghijklmnopqrstuvwxyz1234567890/@!:;#\\s$%^&*()\-_=+.,?'\"{}\[\]|`~<> ]+창"), 
 ]
 
-# banned_patterns에 등록된 분란을 조장하는 단어를 수정한 채팅에서 사용하였을 때 작동함.
-
-async def 금칙어_검사01(content):
-    for pattern in banned_patterns:
-        if pattern.search(content):
-            return True
-    return False
- 
-@app.event
-async def on_message_edit(before, after):
-    if after.author.bot:  # 봇에 의해 보내진 메시지는 무시
-        return
-     
-    if 금칙어_검사01(after.content):  # 금칙어_검사는 금칙어를 검사하는 함수입니다.
-        await after.delete()
-        await after.channel.send(f"{after.author.mention}님, 수정된 메시지에서 금칙어가 검출되었습니다!")
- 
-# banned_patterns02에 등록된 금칙어를 수정한 채팅에서 사용하였을 때 작동함.
-
-async def 금칙어_검사02(content):
-    for pattern in banned_patterns02:
-        if pattern.search(content):
-            return True
-    return False
- 
-@app.event
-async def on_message_edit(before, after):
-    if after.author.bot:  # 봇에 의해 보내진 메시지는 무시
-        return
-     
-    if 금칙어_검사02(after.content):  # 금칙어_검사는 금칙어를 검사하는 함수입니다.
-        await after.delete()
-        await after.channel.send(f"{after.author.mention} 님, 수정된 메시지에서 금칙어가 검출되었습니다!")
-
 @app.event
 async def on_ready():
     print("I'm logging in.")  
